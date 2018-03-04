a1a068a4858fb0432b5e14795177bb5fd935ede5
ramigoldvarg <ramigoldvarg7@gmail.com>
2018-03-04 20:35:40 +0200

    Fix(Admin): Reg page working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const routes = require('./server/routes/index')
 const adminRoutes = require('./server/routes/adminRoutes.js');
+const bodyParser = require('body-parser')
 
 var app = express();
 
@@ -13,7 +14,9 @@ app.use('/admin',adminRoutes)
 
 app.use(express.static(path.join(__dirname, 'public/admin')))
 app.use('/bower_components', express.static(path.join(__dirname, '/public/bower_components')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .use('/', routes)
+  .use('/', express.static(path.join(__dirname, 'views/admin')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
